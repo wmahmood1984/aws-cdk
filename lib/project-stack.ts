cb7b6f4f809ae5046b82136cd2179ac0399fc253
Waqas Mahmood <waqasnizamani3@gmail.com>
2020-11-28 23:26:57 +0500

    step-02 completed

--- lib/project-stack.ts
+++ lib/project-stack.ts
@@ -1,25 +1,33 @@
-// import * as sns from '@aws-cdk/aws-sns';
-// import * as subs from '@aws-cdk/aws-sns-subscriptions';
-// import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
-// import * as s3 from '@aws-cdk/aws-s3';
-import * as lambda from '@aws-cdk/aws-lambda';
-import * as apigw from '@aws-cdk/aws-apigateway';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3deploy from "@aws-cdk/aws-s3-deployment";
 
 export class ProjectStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-     // defines an AWS Lambda resource
-     const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_10_X,    // execution environment
-      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
-      handler: 'hello.handler'                // file is "hello", function is "handler"
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', 
+    {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true
+    }
+    );
+
+    const distribution = new cloudfront.Distribution(this, 'myDist', {
+      defaultBehavior: { origin: new origins.S3Origin(websiteBucket) },
     });
 
-    // defines an API Gateway REST API resource backed by our "hello" function.
-    new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
+    // Prints out the web endpoint to the terminal
+    new cdk.CfnOutput(this, 'DistributionDomainName', {
+      value: distribution.domainName
+    })
+
+    const websiteDeployment = new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset('./website')],
+      destinationBucket: websiteBucket,
+      distribution: distribution
     });
   }
 }
