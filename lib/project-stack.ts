247f1212396b23d7d4f594a2733da48ab0a1927f
Waqas Mahmood <waqasnizamani3@gmail.com>
2020-11-28 19:55:03 +0500

    first committ

---
+++ lib/project-stack.ts
@@ -0,0 +1,15 @@
+import * as sns from '@aws-cdk/aws-sns';
+import * as subs from '@aws-cdk/aws-sns-subscriptions';
+import * as sqs from '@aws-cdk/aws-sqs';
+import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+
+export class ProjectStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true
+    });
+  }
+}
