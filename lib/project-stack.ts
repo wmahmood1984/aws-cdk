2bc986b322ffb7520a234860f6ac2e3528bc94fa
Waqas Mahmood <waqasnizamani3@gmail.com>
2020-11-29 12:20:13 +0500

    completed stage 04 without faker

--- lib/project-stack.ts
+++ lib/project-stack.ts
@@ -1,33 +1,92 @@
+// lib/appsync-cdk-app-stack.ts
 import * as cdk from '@aws-cdk/core';
-import * as cloudfront from '@aws-cdk/aws-cloudfront';
-import * as origins from '@aws-cdk/aws-cloudfront-origins';
-import * as s3 from '@aws-cdk/aws-s3';
-import * as s3deploy from "@aws-cdk/aws-s3-deployment";
+import * as appsync from '@aws-cdk/aws-appsync';
+import * as ddb from '@aws-cdk/aws-dynamodb';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class ProjectStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', 
-    {
-      websiteIndexDocument: 'index.html',
-      publicReadAccess: true
-    }
-    );
-
-    const distribution = new cloudfront.Distribution(this, 'myDist', {
-      defaultBehavior: { origin: new origins.S3Origin(websiteBucket) },
+    // Creates the AppSync API
+    const api = new appsync.GraphqlApi(this, 'Api', {
+      name: 'cdk-notes-appsync-api',
+      schema: appsync.Schema.fromAsset('graphql/schema.graphql'),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            expires: cdk.Expiration.after(cdk.Duration.days(365))
+          }
+        },
+      },
+      xrayEnabled: true,
     });
 
-    // Prints out the web endpoint to the terminal
-    new cdk.CfnOutput(this, 'DistributionDomainName', {
-      value: distribution.domainName
-    })
+    // lib/appsync-cdk-app-stack.ts
+  const notesLambda = new lambda.Function(this, 'AppSyncNotesHandler', {
+  runtime: lambda.Runtime.NODEJS_12_X,
+  handler: 'main.handler',
+  code: lambda.Code.fromAsset('lambda-fns'),
+  memorySize: 1024
+  });
 
-    const websiteDeployment = new s3deploy.BucketDeployment(this, 'DeployWebsite', {
-      sources: [s3deploy.Source.asset('./website')],
-      destinationBucket: websiteBucket,
-      distribution: distribution
+// Set the new Lambda function as a data source for the AppSync API
+const lambdaDs = api.addLambdaDataSource('lambdaDatasource', notesLambda);
+
+// lib/appsync-cdk-app-stack.ts
+lambdaDs.createResolver({
+  typeName: "Query",
+  fieldName: "getNoteById"
+});
+
+lambdaDs.createResolver({
+  typeName: "Query",
+  fieldName: "listNotes"
+});
+
+lambdaDs.createResolver({
+  typeName: "Mutation",
+  fieldName: "createNote"
+});
+
+lambdaDs.createResolver({
+  typeName: "Mutation",
+  fieldName: "deleteNote"
+});
+
+lambdaDs.createResolver({
+  typeName: "Mutation",
+  fieldName: "updateNote"
+});
+
+// lib/appsync-cdk-app-stack.ts
+const notesTable = new ddb.Table(this, 'CDKNotesTable', {
+  billingMode: ddb.BillingMode.PAY_PER_REQUEST,
+  partitionKey: {
+    name: 'id',
+    type: ddb.AttributeType.STRING,
+  },
+});
+// enable the Lambda function to access the DynamoDB table (using IAM)
+notesTable.grantFullAccess(notesLambda)
+
+// Create an environment variable that we will use in the function code
+notesLambda.addEnvironment('NOTES_TABLE', notesTable.tableName);
+
+    // Prints out the AppSync GraphQL endpoint to the terminal
+    new cdk.CfnOutput(this, "GraphQLAPIURL", {
+     value: api.graphqlUrl
+    });
+
+    // Prints out the AppSync GraphQL API key to the terminal
+    new cdk.CfnOutput(this, "GraphQLAPIKey", {
+      value: api.apiKey || ''
+    });
+
+    // Prints out the stack region to the terminal
+    new cdk.CfnOutput(this, "Stack Region", {
+      value: this.region
     });
   }
 }
